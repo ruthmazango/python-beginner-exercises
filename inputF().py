de3ae6c5cf8806c4ee2041b934b1654e6f242382
ruthmazango <ruthmazango@gmail.com>
2021-11-14 17:37:08 +0200

    Add files via upload

---
+++ inputF().py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 14 17:30:44 2021
+
+@author: User
+"""
+
+#____________________________Area of a circle________________________________
+
+radius = float(input('Enter the radius of your circle:> '))
+
+pi = 3.14159
+
+area_of_circle = pi * radius ** 2
+
+print(f'+++++++++++++++++++++\n Area of circle with radius: {radius}, is {area_of_circle}')
---
+++ printF().py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 14 17:23:23 2021
+
+@author: Ruth Mazango
+"""
+
+print("One, 'two',\n \"three\", four, \\five\\, once\n   'I' caught a fish '//alive\\\\'")
